9406f0217b920c6c2c6bf645a1af0d5c23561ec7
Aron Roberts <aron@socrates.berkeley.edu>
2014-01-09 15:38:18 -0800

    CSPACE-6265: Change to double quoting for one variable.

--- manifests/init.pp
+++ manifests/init.pp
@@ -90,7 +90,7 @@ class cspace_java {
       # E.g. gives JDK version '7u45' for Java version 7, update 45
       $jdk_version         = "${java_version}u${update_number}"
       # E.g. gives build version 'b18' for build 18
-      $build_version       = 'b${build_number}'
+      $build_version       = "b${build_number}"
       $jdk_path_segment    = "${jdk_version}-${build_version}"
       $jdk_filename_prefix = "jdk-${jdk_version}"
       $os_bits = $cspace_environment::osbits::os_bits
